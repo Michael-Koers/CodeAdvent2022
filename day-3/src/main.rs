6f9b8126d36ab1ebc0916206eaea1faaed3e531c
Michael <michaelkoers95@gmail.com>
2022-12-05 17:29:09 +0100

    Finished day 3 part 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
         fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     // part 1 - let items = find_items(contents);
-    // part 2
+    // part 2 - find same letter in every 3 lines.
     let items = find_groups(contents);
     let count = count_items(items);
 
@@ -32,25 +32,22 @@ fn find_items(contents: String) -> Vec<char> {
     return results;
 }
 
-
 fn find_groups(contents: String) -> Vec<char> {
     let mut results = Vec::new();
 
+    let lines: Vec<String> = contents.lines().map(|line| line.to_string()).collect();
 
-    // i = current index, item = current item
-    // 'outer: for (i, item) in lines.enumerate(){
-    //     for f in item.chars() {
-    //         if lines.
-    //     }
-    // }
-    // group 3 lines
-    // find letter that is contained in each line
-    // add to char vector
-    // go to next line, that is 3 lines over, so +3
+    'outer: for (i, item) in contents.lines().enumerate().step_by(3) {
+        for f in item.chars() {
+            if lines.get(i + 1).unwrap().contains(f) && lines.get(i + 2).unwrap().contains(f) {
+                results.push(f);
+                continue 'outer;
+            }
+        }
+    }
 
     return results;
 }
-
 
 fn count_items(items: Vec<char>) -> u32 {
     let mut count: u32 = 0;
@@ -58,7 +55,7 @@ fn count_items(items: Vec<char>) -> u32 {
     for item in items {
         if item.is_ascii_lowercase() {
             // minus 96 because 'a' is ascii code 97 in decimals, so to get 'a' as '1' we need to do -96
-            count += (item as u32) - 96 
+            count += (item as u32) - 96
         } else if item.is_ascii_uppercase() {
             // same as above, but now 'A' is 65 in ascii and for this challange it is supposed to be 27, so minus -38
             count += (item as u32) - 38;
