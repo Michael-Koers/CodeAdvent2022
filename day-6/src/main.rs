25d1f51b5d51b9286bc4c742b5ea52ecd75b84c2
Michael <michaelkoers95@gmail.com>
2022-12-07 21:54:15 +0100

    Finished day 6 part 2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     let file_path: &str = "input.txt";
     println!("Reading file {}", file_path);
 
+    // for day 1 solution, use marker_length = 4
+    // for day 2 solution, use marker_length = 14
+    let marker_length = 14;
+
     let contents: String =
         fs::read_to_string(file_path).expect("Should have been able to read the file");
 
@@ -15,14 +19,14 @@ fn main() {
         let mut set: HashSet<&str> = HashSet::new();
 
         // 0..3 is exclusive, =3 is inclusive end of range
-        for r in 0..=3 {
+        for r in 0..marker_length {
             set.insert(chars[index + r]);
         }
 
         println!("Attempt {}: {:?}", index, set);
 
-        if set.len() == 4 {
-            println!("First marker after {}", index + 4);
+        if set.len() == marker_length {
+            println!("First marker after {}", index + marker_length);
             break;
         }
     }
