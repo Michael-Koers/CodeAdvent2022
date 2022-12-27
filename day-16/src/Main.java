a2622fc2209a41af1760a888370d323a05972ac0
Michael <michaelkoers95@gmail.com>
2022-12-27 14:36:38 +0100

    Finished day 16 part 1, working on part 2

--- day-16/src/Main.java
+++ day-16/src/Main.java
@@ -6,155 +6,220 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
-        String file = "day-16/input-test.txt";
+        Map<String, Valve> valves = parseInput("input.txt");
 
-        List<Valve> valves = parseInput(file);
+        Map<Route, Integer> costs = calculateCosts(valves);
 
-        Valve start = valves.stream().filter(v -> v.name.equals("AA")).findFirst().orElseThrow(NoSuchElementException::new);
-        List<Valve> usefulValves = valves.stream().filter(v -> v.flowRate > 0 || v.name.equals("AA")).collect(Collectors.toList());
+        // Puzzle 1
+//        State best = openValves(valves, costs);
 
-        List<Distance> distances = findDistances(usefulValves, valves);
-        int pressure = findMostPressure(start, 30, new ArrayList<>(), usefulValves, distances);
+        // Puzzle 2
+        StateP2 bestWithElephant = openValvesWithElephant(valves, costs);
 
-        System.out.printf("Most released pressure: %s%n", pressure);
+//        System.out.printf("Best path leads to %s pressure released%n", best.pressure());
+        System.out.printf("Best path with elephant leads to %s pressure released%n", bestWithElephant.pressure());
     }
 
-    private static int findMostPressure(Valve start, int time, List<Valve> seenValves, List<Valve> usefulValves, List<Distance> distances) {
+    private static StateP2 openValvesWithElephant(Map<String, Valve> valves, Map<Route, Integer> costs) {
 
-        seenValves.add(start);
-        usefulValves.removeAll(seenValves);
+        Player human = new Player("AA", 26);
+        Player elephant = new Player("AA", 26);
 
-        int bestFlow = 0;
-        int timeLeft;
+        LinkedList<StateP2> attempts = new LinkedList<>();
+        attempts.add(new StateP2(human, elephant, 0, new ArrayList<>()));
 
-        for (String v : start.neighbours){
-            if (usefulValves.stream().noneMatch(x -> x.name.equals(v))){
-                continue;
+        StateP2 best = null;
+
+        while (!attempts.isEmpty()) {
+            StateP2 attempt = attempts.poll();
+
+            // Determine who to move first
+            boolean moveHuman = attempt.human().timeLeft() >= attempt.elephant().timeLeft();
+
+            // If the one can't move anymore, the other still might
+            boolean canMove = movePlayer(attempt, attempts, valves, costs, moveHuman);
+
+            if (!canMove) {
+                canMove = movePlayer(attempt, attempts, valves, costs, !moveHuman);
             }
 
-            Valve currV = getValve(usefulValves, v);
-
-            Distance d = distances.stream()
-                    .filter(dis -> dis.start.equals(start.name) && dis.end.equals(currV.name))
-                    .findFirst()
-                    .orElseThrow(NoSuchElementException::new);
-
-            timeLeft = time - d.distance - 1;
-
-            if (timeLeft > 0){
-                int flow = currV.flowRate * timeLeft;
-                flow += findMostPressure(currV, timeLeft, seenValves, usefulValves, distances);
-                bestFlow = Math.max(bestFlow, flow);
+            // If both can't move anymore, check if result is higher than best known result
+            if (!canMove) {
+                if (best == null || attempt.pressure() > best.pressure()) {
+                    best = attempt;
+                }
             }
         }
-        return  bestFlow;
+
+        return best;
     }
 
-    private static List<Distance> findDistances(List<Valve> usefulValves, List<Valve> valves) {
+    private static boolean movePlayer(StateP2 attempt, List<StateP2> attempts, Map<String, Valve> valves, Map<Route, Integer> costs, boolean isHuman) {
 
-        List<Distance> distances = new ArrayList<>();
+        Player toMove = isHuman ? attempt.human() : attempt.elephant();
 
-        for (Valve v : valves) {
+        List<StateP2> newStates = costs.entrySet().stream()
+                .filter(e -> e.getKey().from().equals(toMove.current()))
+                .filter(e -> !attempt.opened().contains(e.getKey().to()))
+                .filter(e -> toMove.timeLeft() >= e.getValue() + 1)
+                .map(e -> attempt.moveToAndOpen(toMove, e.getKey().to(), e.getValue(), valves.get(e.getKey().to()).flowRate, isHuman))
+                .collect(Collectors.toList());
 
-            if (!usefulValves.contains(v)) {
-                continue;
+        // Add new states to start of the list as to prevent OutOfMemory
+        attempts.addAll(0, newStates);
+        return !newStates.isEmpty();
+    }
+
+    private static State openValves(Map<String, Valve> valves, Map<Route, Integer> costs) {
+
+        LinkedList<State> attempts = new LinkedList<>();
+        attempts.add(new State("AA", 30, 0, new ArrayList<>()));
+
+        State best = null;
+
+        while (!attempts.isEmpty()) {
+            State state = attempts.poll();
+
+            List<State> newStates = costs.entrySet().stream()
+                    // Filter to route from current valve
+                    .filter(e -> e.getKey().from().equals(state.current()))
+                    // We haven't visited the valve yet
+                    .filter(e -> !state.opened().contains(e.getKey().to()))
+                    // We can afford to go to the valve
+                    .filter(e -> state.timeLeft() >= e.getValue() + 1)
+                    // Create new states for each possibility
+                    .map(e -> state.moveToAndOpen(e.getKey().to(), e.getValue(), valves.get(e.getKey().to()).flowRate))
+                    .collect(Collectors.toList());
+
+            if (newStates.isEmpty()) {
+                if (best == null || state.pressure() > best.pressure()) {
+                    best = state;
+                }
             }
+            attempts.addAll(newStates);
+        }
+        return best;
+    }
 
-            List<Valve> currentValves = Collections.singletonList(v);
-            List<Valve> nextValves = new ArrayList<>();
-            int distance = 0;
+    private static Map<Route, Integer> calculateCosts(Map<String, Valve> valves) {
+        // Floyd Warshall this bitch
+        Map<Route, Integer> costs = new HashMap<>();
 
-            distances.add(new Distance(v.name, v.name, 0));
+        // Get all the basic distances in, the ones we already know about
+        for (Valve valve : valves.values()) {
+            valve.neighbour.forEach(target -> costs.put(new Route(valve.name, target), 1));
+        }
 
-            while (!currentValves.isEmpty()) {
-                distance++;
+        // Not exactly sure how the three for-loops work
+        for (Valve via : valves.values()) {
+            for (Valve source : valves.values()) {
+                for (Valve dest : valves.values()) {
+                    if (via == source || source == dest || dest == via) {
+                        continue;
+                    }
 
-                for (Valve current : currentValves) {
-                    for (String e : current.neighbours) {
-                        Valve exit = getValve(valves, e);
+                    // Calcute distance via the intermediate node (valve)
+                    Route part1 = new Route(source.name, via.name);
+                    Route part2 = new Route(via.name, dest.name);
 
-                        Distance currDistance = new Distance(current.name, exit.name, distance);
-                        if (distances.contains(currDistance)) {
-                            continue;
+                    // If routes are known (if not, it is a non-existent edge)
+                    if (costs.containsKey(part1) && costs.containsKey(part2)) {
+
+                        Route directRoute = new Route(source.name, dest.name);
+
+                        // Check whether this new found connection from source to destination is faster
+                        // than either an existing connection or INFINITY.
+                        Integer directCost = costs.get(part1) + costs.get(part2);
+                        if (directCost < costs.getOrDefault(directRoute, Integer.MAX_VALUE)) {
+                            costs.put(directRoute, directCost);
                         }
-
-                        distances.add(currDistance);
-                        nextValves.add(exit);
-
                     }
                 }
-                currentValves = nextValves;
-                nextValves = new ArrayList<>();
             }
         }
-        return distances;
+
+        // Remove valves that are not interesting to visit as a source/destination because they don't provide any flowrate
+        List<String> irrelevantValves = valves.values().stream()
+                .filter(v -> v.flowRate == 0 && !v.name.equals("AA"))
+                .map(v -> v.name)
+                .toList();
+        List<Route> irrelevantRoutes = costs.keySet().stream().filter(route -> irrelevantValves.contains(route.to()) || irrelevantValves.contains(route.from())).toList();
+        irrelevantRoutes.forEach(costs::remove);
+
+        return costs;
     }
 
-    private static List<Valve> parseInput(String file) throws IOException {
+    private static Map<String, Valve> parseInput(String s) throws IOException {
 
-        List<Valve> valves = new ArrayList<>();
+        Map<String, Valve> valves = new HashMap<>();
 
-        for (String line : Files.readAllLines(Paths.get(file))) {
+        for (String line : Files.readAllLines(Paths.get("day-16/" + s))) {
 
-            String[] valveInfo = line.split(";")[0].split(" ");
-            List<String> tunnelInfo = Arrays.stream(line.split(";")[1]
-                    // Cuz im too lazy to use Regex
+            String valveInfo = line.split(";")[0];
+            List<String> neighbours = Arrays.stream(line.split(";")[1]
                     .replace("tunnels lead to valves ", "")
                     .replace("tunnel leads to valve ", "")
                     .trim()
-                    .split(", ")
-            ).toList();
+                    .split(", "))
+                    .toList();
 
-            valves.add(new Valve(valveInfo[1], Integer.parseInt(valveInfo[4].split("=")[1]), tunnelInfo));
-
+            valves.put(valveInfo.split(" ")[1], new Valve(valveInfo.split(" ")[1], Integer.parseInt(valveInfo.split("=")[1]), neighbours));
         }
-
         return valves;
-    }
-
-    private static Valve getValve(List<Valve> valves, String name) {
-        return valves.stream().filter(v -> v.name.equals(name)).findFirst().orElseThrow(NoSuchElementException::new);
     }
 }
 
+record State(String current, int timeLeft, int pressure, List<String> opened) {
+
+    State moveToAndOpen(String destination, int timeCost, int flowRate) {
+        List<String> opened = new ArrayList<>(this.opened);
+        opened.add(destination);
+
+        int timeLeft = this.timeLeft - timeCost - 1;
+        int pressure = this.pressure + (flowRate * timeLeft);
+
+        return new State(destination, timeLeft, pressure, opened);
+    }
+}
+
+record Player(String current, int timeLeft) {
+};
+
+record StateP2(Player human, Player elephant, int pressure, List<String> opened) {
+    StateP2 moveToAndOpen(Player player, String destination, int timeCost, int flowRate, boolean isHuman) {
+        List<String> opened = new ArrayList<>(this.opened);
+        opened.add(destination);
+
+        int timeLeft = player.timeLeft() - timeCost - 1;
+        int pressure = this.pressure + (flowRate * (player.timeLeft() - timeCost - 1));
+
+        Player human = this.human;
+        Player elephant = this.elephant;
+
+        if (isHuman) {
+            human = new Player(destination, timeLeft);
+        } else {
+            elephant = new Player(destination, timeLeft);
+        }
+
+        return new StateP2(human, elephant, pressure, opened);
+    }
+};
+
+record Route(String from, String to) {
+}
 
 class Valve {
 
-    final int flowRate;
     final String name;
-    final List<String> neighbours;
-    boolean isClosed = true;
+    final int flowRate;
+    final List<String> neighbour;
 
-    public Valve(String name, int flowRate, List<String> neighbours) {
-        this.flowRate = flowRate;
+    Valve(String name, int flowRate, List<String> neighbour) {
         this.name = name;
-        this.neighbours = neighbours;
-    }
-}
-
-class Distance {
-    final String start;
-    final String end;
-    final int distance;
-
-    public Distance(String start, String end, int distance) {
-        this.start = start;
-        this.end = end;
-        this.distance = distance;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Distance distance = (Distance) o;
-        return Objects.equals(start, distance.start) && Objects.equals(end, distance.end);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(start, end);
+        this.flowRate = flowRate;
+        this.neighbour = neighbour;
     }
 }
